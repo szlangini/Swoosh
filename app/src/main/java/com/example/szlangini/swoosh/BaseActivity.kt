2b84c9c6939417b804ab7d957ae2a6561a93584b
szlangini <vincent.szlang@googlemail.com>
2019-01-12 19:09:31 +0100

    added Skill Layout and some logic

---
+++ app/src/main/java/com/example/szlangini/swoosh/BaseActivity.kt
@@ -0,0 +1,57 @@
+package com.example.szlangini.swoosh
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log
+
+
+open class BaseActivity : AppCompatActivity() {
+
+    val TAG = "LifeCycle"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "${javaClass.simpleName}, onCreate")
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "${javaClass.simpleName}, onStart")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "${javaClass.simpleName}, onResume")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "${javaClass.simpleName}, onPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "${javaClass.simpleName}, onStop")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "${javaClass.simpleName}, onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "${javaClass.simpleName}, onRestart")
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/szlangini/swoosh/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.szlangini.swoosh
+
+const val EXTRA_LEAGUE = "league"
